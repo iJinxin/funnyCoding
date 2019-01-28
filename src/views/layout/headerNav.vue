1dbb467480d3afc6908cca1dc65ba35c32b41294
ijinxin <874450097@qq.com>
2019-01-28 17:07:28 +0800

    header增加github跳转地址

--- src/views/layout/headerNav.vue
+++ src/views/layout/headerNav.vue
@@ -1,18 +1,32 @@
 <template>
   <header>
-    <div class="toggle-menu pointer" @click="toggleMenu()">
-      <i class="iconfont ali-icon-caidan"></i>
+    <div class="toggle-menu float_left pointer" @click="toggleMenu()">
+      <i class="iconfont" :class="{'ali-icon-expand': isCollapse, 'ali-icon-collapse': !isCollapse}"></i>
+    </div>
+    <div class="userinfo float_right pointer" title="view on github" @click="viewGithub()">
+      <i class="iconfont ali-icon-git"></i>
     </div>
   </header>
 </template>
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   data() {
     return {}
   },
+  computed: {
+    ...mapGetters(['sidebarCollapse']),
+    isCollapse() {
+      return this.sidebarCollapse
+    }
+  },
   methods: {
     toggleMenu() {
       this.$store.dispatch("toggleSidebar")
+    },
+    viewGithub() {
+      window.open('https://github.com/iJinxin/funnyCoding')
     }
   }
 }
@@ -27,6 +41,17 @@ header {
   line-height: 48px;
   padding: 0 15px;
   float: left;
+  > i {
+    font-size: 20px;
+  }
+}
+.userinfo {
+  height: 48px;
+  line-height: 48px;
+  padding: 0 15px;
+  > i {
+    font-size: 20px;
+  }
 }
 </style>
 
