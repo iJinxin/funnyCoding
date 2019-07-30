dc864a4eb8dc84731824f82aeea7be9c66964cf5
ijinxin <874450097@qq.com>
2019-07-30 17:54:05 +0800

    api modules

---
+++ src/api/user.js
@@ -0,0 +1,17 @@
+import request from '@/utils/request.js'
+
+export function login(data) {
+  return request({
+    url: "",
+    method: "post",
+    data
+  })
+}
+
+export function logout(data) {
+  return request({
+    url: "",
+    method: "post",
+    data
+  })
+}
