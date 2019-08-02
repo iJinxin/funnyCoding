779556186b74e689f8180db34d67c91e0c4b3779
ijinxin <874450097@qq.com>
2019-08-02 16:57:52 +0800

    test ts-component

---
+++ src/views/layout/components/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header-layout">1</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+  name: 'header'
+})
+export default class extends Vue {
+  get key() {
+    return this.$route.path
+  }
+  private async logout() {
+    return 1;
+  }
+}
+</script>
